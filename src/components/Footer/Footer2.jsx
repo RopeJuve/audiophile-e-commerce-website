7f2d55f8db3734b8af82e3067c5336b671ea581a
RopeJuve <rope.s@hotmail.com>
2023-04-07 16:29:11 +0200

    Footer2

--- src/components/Footer/Footer2.jsx
+++ src/components/Footer/Footer2.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import "./_Footer2.scss";
 
 const Footer2 = () => {
   return (
-    <div>Footer2</div>
-  )
-}
+    <div className="footer2__container">
+      <h6>audiophile</h6>
+      <p>
+        Audiophile is an all in one stop to fulfill your audio needs. We're a
+        small team of music lovers and sound specialists who are devoted to
+        helping you get the most out of personal audio. Come and visit our demo
+        facility - we’re open 7 days a week.
+      </p>
+    </div>
+  );
+};
 
-export default Footer2
+export default Footer2;
